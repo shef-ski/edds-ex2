8608d68c55b284cd73db0e71ef4041fab3a5eee2
Leon <leon.olszewski99@gmail.com>
2024-12-22 18:47:37 +0100

    completed sensitivity labeling

--- src/data_processing/load.py
+++ src/data_processing/load.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import os
+
+# Default paths
+path_train = "data/OHSUMED/train-00000-of-00001.parquet"
+path_train = os.path.abspath(path_train)
+
+path_test = "data/OHSUMED/test-00000-of-00001.parquet"
+path_test = os.path.abspath(path_test)
 
 
-path = "data/OHSUMED/train-00000-of-00001.parquet"
+def load_parquet_as_df(data_path: str) -> pd.DataFrame:
+    df = pd.read_parquet(data_path)
+    return df
 
